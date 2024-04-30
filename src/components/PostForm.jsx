753bcccf831e1257fa2f7f8f548615c92186c818
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-04-30 13:20:03 +0200

    commit

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { baseApiUrl } from "../constants.js";
+import { Container } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
 
 const PostForm = () => {
   const [newTitle, setNewTitle] = useState("");
@@ -35,17 +37,20 @@ const PostForm = () => {
   };
 
   return (
-    <div>
-      <h2>Add a New Post</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Blog title:</label>
+    <Container>
+      <h2 className="text-center">Crea un nuovo post!</h2>
+      <form className="d-flex flex-column w-50 mx-auto" onSubmit={handleSubmit}>
+        <label className="my-3">Titolo:</label>
         <input type="text" required value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
-        <label>Blog body:</label>
+        <label className="my-3">Contenuto:</label>
         <textarea required value={newContent} onChange={(e) => setNewContent(e.target.value)}></textarea>
-        <label>Blog author:</label>
-        <button>Publish</button>
+        <label>Titolo:</label>
+
+        <div>
+          <button className="btn btn-success my-5">Pubblica</button>
+        </div>
       </form>
-    </div>
+    </Container>
   );
 };
 
